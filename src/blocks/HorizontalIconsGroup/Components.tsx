3d6895fdbae0f4ee628e3dab0a55037de5bd4342
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2024-10-27 12:19:27 +0100

    added swiper

--- src/blocks/HorizontalIconsGroup/Components.tsx
+++ src/blocks/HorizontalIconsGroup/Components.tsx
@@ -1,7 +1,15 @@
+'use client'
+
 import React from 'react'
 import { MdSupportAgent } from 'react-icons/md'
 import { BsPersonBoundingBox } from 'react-icons/bs'
 import { FaRegRectangleList } from 'react-icons/fa6'
+import Slider from 'react-slick'
+
+import { cn } from '@/utilities/cn'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 import RichText from '@/components/RichText'
 
@@ -16,22 +24,56 @@ interface HorizontalIconsGroupProps {
 }
 
 export const HorizontalIconsGroup = (props: HorizontalIconsGroupProps) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dotsClass: 'slick-dots slick-dots-horizontal-icons-group',
+  }
+
   return (
-    <div className="w-full container-spacing">
-      <div className="container-wrapper">
-        {props.pretitle && (
-          <h3 className="text-sm font-bold text-shark-400 pb-6 uppercase">{props.pretitle}</h3>
-        )}
-        {props.title && (
-          <h2 className="md:text-6xl text-3xl font-bold text-astral-900 pb-16">{props.title}</h2>
-        )}
-        <div className="grid md:grid-cols-3 gap-8 md:gap-16">
-          {props.items &&
-            props.items.map((item, index) => (
+    <>
+      <div className="w-full container-spacing !pb-0 md:!pb-16">
+        <div className="container-wrapper">
+          {props.pretitle && (
+            <h3 className="text-sm font-bold text-shark-400 pb-6 uppercase">{props.pretitle}</h3>
+          )}
+          {props.title && (
+            <h2 className="md:text-6xl text-3xl font-bold text-astral-900 pb-16">{props.title}</h2>
+          )}
+          <div className=" grid-cols-3 gap-16 hidden md:grid">
+            {props.items &&
+              props.items.map((item, index) => (
+                <HomeWhyUsItem
+                  key={index}
+                  title={item.title}
+                  description={item.description}
+                  icon={
+                    item.icon === 'MdSupportAgent' ? (
+                      <MdSupportAgent className="w-6 h-6" />
+                    ) : item.icon === 'BsPersonBoundingBox' ? (
+                      <BsPersonBoundingBox className="w-6 h-6" />
+                    ) : (
+                      <FaRegRectangleList className="w-6 h-6" />
+                    )
+                  }
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+      <div className="md:hidden pb-24">
+        <Slider {...settings}>
+          {props.items.map((item, index) => (
+            <div key={index} className="px-8">
               <HomeWhyUsItem
-                key={index}
-                title={item.title}
-                description={item.description}
+                {...item}
+                align="center"
                 icon={
                   item.icon === 'MdSupportAgent' ? (
                     <MdSupportAgent className="w-6 h-6" />
@@ -42,10 +84,11 @@ export const HorizontalIconsGroup = (props: HorizontalIconsGroupProps) => {
                   )
                 }
               />
-            ))}
-        </div>
+            </div>
+          ))}
+        </Slider>
       </div>
-    </div>
+    </>
   )
 }
 
@@ -53,11 +96,19 @@ type HomeWhyUsItemProps = {
   title: string
   description: Record<string, any>
   icon: React.ReactNode
+  className?: string
+  align?: 'left' | 'center'
 }
 
 const HomeWhyUsItem = (props: HomeWhyUsItemProps) => {
   return (
-    <div className="md:border-l-2 border-shark-100 md:pl-10 py-4 md:py-10">
+    <div
+      className={cn(
+        'md:border-l-2 border-shark-100 md:pl-10 py-4 md:py-10',
+        props.className,
+        props.align === 'center' && 'flex flex-col items-center',
+      )}
+    >
       <div className="bg-jaffa-400 rounded-full w-16 h-16 flex items-center justify-center text-white text-2xl mb-4 md:mb-12 shadow-lg shadow-jaffa-400/50">
         {props.icon}
       </div>
@@ -65,7 +116,10 @@ const HomeWhyUsItem = (props: HomeWhyUsItemProps) => {
       <RichText
         content={props.description}
         enableGutter={false}
-        className="text-lg text-shark-500 text-balance"
+        className={cn(
+          'text-lg text-shark-500 text-balance',
+          props.align === 'center' && 'text-center',
+        )}
       />
     </div>
   )
