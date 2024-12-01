7871c35ed29dc525818e68d18684bb11fc40bed3
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2024-12-01 13:14:54 +0100

    test

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -14,6 +14,7 @@ import { authenticated } from '../access/authenticated'
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
+// test
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
