d2837a690db7e4173ad1185488e7225190e7fa1f
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2024-12-06 12:57:48 +0100

    refactor: update Logo component to accept className prop for flexible styling

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { cn } from '@/utilities/cn'
 
-export const Logo = () => {
+type LogoProps = {
+  className?: string
+}
+export const Logo = (props: LogoProps) => {
   return (
-    <span className="text-3xl text-astral-900">
+    <span className={cn('text-3xl text-astral-900', props.className)}>
       <span className="text-primary font-black">TPS</span>
       ervice
     </span>
