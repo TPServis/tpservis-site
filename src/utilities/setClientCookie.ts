97e009904780b47ec774941ad183f65a8978967f
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2024-12-15 22:35:41 +0100

    fix: add a warning for server-side usage in setClientCookie function to prevent misuse in non-client environments

--- src/utilities/setClientCookie.ts
+++ src/utilities/setClientCookie.ts
@@ -3,5 +3,6 @@ export const setClientCookie = (name: string, value: string) => {
     console.warn('setClientCookie can only be used in client-side code')
     return
   }
+
   document.cookie = `${name}=${encodeURIComponent(value)}; path=/; SameSite=Lax; max-age=31536000`
 }
