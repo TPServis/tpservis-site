885f6394b33fd8bb1212267b16dbb585eed5b3bf
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2024-12-15 18:54:47 +0100

    refactor: enhance CustomForm layout and styling; adjust spacing, text classes, and order of elements for improved responsiveness and visual consistency

--- src/blocks/CustomForm/Component.tsx
+++ src/blocks/CustomForm/Component.tsx
@@ -26,19 +26,19 @@ export const CustomForm = async (props: CustomFormBlockProps) => {
     <div className="container-spacing">
       <div className="container-wrapper">
         <section className={cn('py-10 px-4 md:py-16 md:px-6 lg:py-20')}>
-          <div className="lg:w-2/3 lg:ml-auto">
-            <h2 className="text-heading text-4xl font-bold mb-10">{props.heading}</h2>
+          <div className="lg:w-2/3 lg:ml-auto lg:pl-6 mb-10">
+            <h2 className="text-heading text-4xl font-bold mb-10 text-balance">{props.heading}</h2>
             <RichText
               content={props.description}
               enableGutter={false}
-              className="text-lg text-shark-400 pb-6"
+              className="rich-text text-pretty"
             />
           </div>
-          <div className="lg:flex lg:flex-row lg:gap-10 ">
-            <div className="lg:w-1/3">
+          <div className="flex flex-col lg:flex-row gap-10 ">
+            <div className="lg:w-1/3 order-2 lg:order-0">
               <Contacts {...contacts} />
             </div>
-            <div className="lg:w-2/3">
+            <div className="lg:w-2/3 lg:order-1">
               <RenderBlocks blocks={props.formBlock} />
             </div>
           </div>
