e4a62d3f73f05140a98470f522ae26bf3b90f7df
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2025-01-01 14:19:41 +0100

    feat: enhance PlusMinus block with improved layout and icon integration for better visual representation

--- src/blocks/PlusMinus/Component.tsx
+++ src/blocks/PlusMinus/Component.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import RichText from '@/components/RichText'
-import useEmblaCarousel from 'embla-carousel-react'
-// import { RenderBlocks } from '../RenderBlocks'
+import { ThumbsUp, ThumbsDown } from 'lucide-react'
+import { cva } from 'class-variance-authority'
 
 export type PlusMinusBlock = {
   blockType: 'plusMinus'
@@ -14,21 +14,37 @@ export type PlusMinusBlock = {
   id: string
 }
 
-const PlusMinus = (props: PlusMinusBlock) => {
-  const [emblaRef] = useEmblaCarousel()
+const cardVariants = cva(['border rounded-lg p-4 flex-1 w-1/3 min-w-[200px]'], {
+  variants: {
+    value: {
+      plus: 'border-green-500/20 bg-green-50',
+      minus: 'border-red-500/20 bg-red-50',
+    },
+  },
+})
 
+const iconVariants = cva(['text-2xl font-semibold pb-4 pt-8'], {
+  variants: {
+    value: {
+      plus: 'text-green-500',
+      minus: 'text-red-500',
+    },
+  },
+})
+
+const PlusMinus = (props: PlusMinusBlock) => {
   return (
-    <div>
-      <h2>{props.heading}</h2>
-      <div className="embla" ref={emblaRef}>
-        <div className="embla__container">
-          {props.card.map((card) => (
-            <div key={card.id} className="embla__slide">
-              <h3>{card.value}</h3>
-              <RichText content={card.content} className="text-shark-600 prose rich-text" />
-            </div>
-          ))}
-        </div>
+    <div className="!mb-8">
+      <h3 className="text-2xl font-semibold text-text-heading pb-4 pt-8">{props.heading}</h3>
+      <div className="flex flex-wrap gap-4">
+        {props.card.map((card) => (
+          <div key={card.id} className={cardVariants({ value: card.value })}>
+            <h4 className={iconVariants({ value: card.value })}>
+              {card.value === 'plus' ? <ThumbsUp /> : <ThumbsDown />}
+            </h4>
+            <RichText content={card.content} className="text-shark-600 prose rich-text" />
+          </div>
+        ))}
       </div>
     </div>
   )
