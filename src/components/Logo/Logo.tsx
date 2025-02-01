d314223f3d7dea276cef9a5c8b2b43b8a432197c
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2025-02-01 17:36:10 +0100

    style: remove text color class from Logo component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -6,7 +6,7 @@ type LogoProps = {
 export const Logo = (props: LogoProps) => {
   return (
     <span className={cn('text-3xl text-astral-900', props.className)}>
-      <span className="text-primary font-black">TPS</span>
+      <span className="font-black">TPS</span>
       ervice
     </span>
   )
