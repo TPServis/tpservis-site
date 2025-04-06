4cfd5637e7ab7b14c648fe58fbba9662b44ab99b
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2025-04-06 16:51:55 +0200

    feat: Enhance RoomDrawer component with new layout and styling, including header, title, and improved content structure for better user experience

--- src/blocks/TourSearchModule/RoomDrawer.tsx
+++ src/blocks/TourSearchModule/RoomDrawer.tsx
@@ -8,7 +8,10 @@ import {
   DrawerClose,
   DrawerContent,
   DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
   DrawerTrigger,
+  DrawerDescription,
 } from '@/components/ui/drawer'
 import { Room } from './types'
 
@@ -21,7 +24,7 @@ const RoomDrawer = ({ children, room }: RoomDrawerProps): JSX.Element => {
   const [open, setOpen] = useState(false)
 
   return (
-    <Drawer open={open} onOpenChange={setOpen}>
+    <Drawer open={open} onOpenChange={setOpen} shouldScaleBackground={true}>
       <DrawerTrigger asChild>
         <button
           aria-label="order"
@@ -30,13 +33,32 @@ const RoomDrawer = ({ children, room }: RoomDrawerProps): JSX.Element => {
           {children}
         </button>
       </DrawerTrigger>
-      <DrawerContent>
-        <p>{room.title}</p>
-        <DrawerFooter className="pt-2">
-          <DrawerClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
+      <DrawerContent className="h-screen !bg-astral-50 border-none">
+        <div>
+          <DrawerHeader>
+            <DrawerTitle className="text-astral-900 font-bold text-4xl">{room.title}</DrawerTitle>
+          </DrawerHeader>
+          <div className="flex flex-col gap-8 p-4 h-screen">
+            <div className="flex flex-col gap-2">
+              <p className="text-sm">Ціна за номер</p>
+              <p className="text-3xl font-bold">{room.price_uah.toLocaleString('uk-UA')} грн</p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <p className="text-sm">Тип харчування</p>
+              <p className="text-3xl font-bold">{room.meal_type}</p>
+            </div>
+          </div>
+          <DrawerFooter className="pt-2">
+            <DrawerClose asChild>
+              <button
+                aria-label="close"
+                className="w-full bg-astral-600 text-astral-50 rounded-xl p-2 font-bold"
+              >
+                Закрити
+              </button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
       </DrawerContent>
     </Drawer>
   )
