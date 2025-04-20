e20c7b934658d69cec536c5032776a08d7f286e4
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2025-04-20 11:02:16 +0200

    Refactor HotelGallery component for improved layout and responsiveness, adjusting image display and button functionality.

--- src/blocks/TourSearchModule/HotelGallery.tsx
+++ src/blocks/TourSearchModule/HotelGallery.tsx
@@ -31,64 +31,60 @@ export const HotelGallery = ({ images }: Props): JSX.Element => {
   }
 
   return (
-    <div>
-      <button
-        type='button'
-        className='relative group w-60 cursor-pointer'
-        onClick={(): void => handleToggle()}
-      >
-        {Array.from(images)
-          .splice(0, 3)
-          .reverse()
-          .map((image, index) => (
-            <div
-              key={image}
-              className='border-10 border-white shadow-sm rounded-2xl overflow-hidden not-first:absolute not-first:inset-0 bg-white group-hover:last:-rotate-6 transition origin-top-left group-hover:first:rotate-6 aspect-[4/3]'
-            >
-              <Image
-                src={image}
-                alt={`Hotel ${index + 1}`}
-                width={300}
-                height={200}
-                className='w-full h-full object-cover'
-              />
-            </div>
-          ))}
-      </button>
-      {isOpen && (
-        <div className='fixed inset-0 bg-black/20 z-1000 flex justify-center items-center'>
-          <button
-            type='button'
-            className='absolute inset-0 bg-transparent'
-            onClick={(): void => handleToggle()}
-          >
-            {' '}
-            close{' '}
-          </button>
+			<div>
+				<button
+					type="button"
+					className="relative group w-60 cursor-pointer"
+					onClick={(): void => handleToggle()}
+				>
+					{Array.from(images)
+						.splice(0, 3)
+						.reverse()
+						.map((image, index) => (
+							<div
+								key={image}
+								className="border-10 border-white shadow-sm rounded-2xl overflow-hidden not-first:absolute not-first:inset-0 bg-white group-hover:last:-rotate-6 transition origin-top-left group-hover:first:rotate-6 aspect-[4/3]"
+							>
+								<Image
+									src={image}
+									alt={`Hotel ${index + 1}`}
+									width={300}
+									height={200}
+									className="w-full h-full object-cover"
+								/>
+							</div>
+						))}
+				</button>
+				{isOpen && (
+					<div className="fixed inset-0 bg-black/20 z-1000 flex justify-center items-center">
+						<button
+							type="button"
+							className="absolute inset-0 bg-transparent"
+							onClick={(): void => handleToggle()}
+						>
+							{" "}
+							close{" "}
+						</button>
 
-          <div
-            className='aspect-[4/3] w-[60vw] border-4 border-white'
-            ref={emblaRef}
-          >
-            <div className='flex gap-4'>
-              {images.map((image, index) => (
-                <div
-                  key={image}
-                  className='w-full h-auto shrink-0'
-                >
-                  <Image
-                    src={image}
-                    alt={`Hotel ${index + 1}`}
-                    width={300}
-                    height={200}
-                    className='w-full h-full object-cover'
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  )
+						<div className="w-[60vw] h-[80vh]" ref={emblaRef}>
+							<div className="flex gap-4">
+								{images.map(
+									(image, index): JSX.Element => (
+										<div key={image} className="w-full h-auto shrink-0 border-8 border-white">
+											<Image
+												src={image}
+												alt={`Hotel ${index + 1}`}
+												width={1000}
+												height={600}
+												className="w-full h-full object-contain"
+											/>
+										</div>
+									),
+								)}
+							</div>
+						</div>
+					</div>
+				)}
+			</div>
+		);
 }
